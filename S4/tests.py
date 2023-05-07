0c59700468e537c46622cb2e52baf84fc666b953
necrashter <iiilker99@gmail.com>
2023-05-07 21:23:05 +0300

    Test the equivalence of convolutional and recurrent method

--- S4/tests.py
+++ S4/tests.py
@@ -1,6 +1,7 @@
 import unittest
 from __init__ import *
 
+
 class TestS4Components(unittest.TestCase):
     def test_HiPPO_NPLR_DPLR(self):
         """
@@ -35,9 +36,37 @@ class TestS4Components(unittest.TestCase):
         a = conv_kernel_naive(Ab, Bb, Cb.conj(), L)
 
         # Compare to the DPLR generating function approach.
-        C = (torch.eye(4) - torch.linalg.matrix_power(Ab, L)).conj().T @ Cb.ravel()
+        C = (torch.eye(4) - torch.linalg.matrix_power(Ab, L)).conj().T @ Cb.flatten()
         b = conv_kernel_DPLR(Lambda, P, P, B, C, step=1.0 / L, L=L)
-        self.assertTrue(torch.allclose(a.real, b.real))
+        self.assertTrue(torch.allclose(a.real, b.real, atol=1e-5, rtol=1e-5))
+
+    def test_conv_and_recurrent(self):
+        """
+        Test whether the result of convolution is equivalent to the recurrent method.
+        """
+        L = 16
+        Lambda, _, P, B = init_DPLR_HiPPO(4)
+
+        # Discretize HiPPO
+        A = torch.diag(Lambda) - P.unsqueeze(1) @ P.unsqueeze(1).conj().T
+        C = torch.randn(1, 4, dtype=torch.complex64)
+        Ab, Bb, Cb = discretize_SSM(A, B, C, 1.0 / L)
+
+        # Recurrent method
+        u = torch.randn(L)
+        recurrent_out = run_recurrent_SSM(Ab, Bb, Cb.conj(), u.unsqueeze(1))[1]
+
+        # Convolutional method
+        # Naive kernel
+        K_naive = conv_kernel_naive(Ab, Bb, Cb.conj(), L)
+        # DPLR convolution kernel
+        C = (torch.eye(4) - torch.linalg.matrix_power(Ab, L)).conj().T @ Cb.flatten()
+        K = conv_kernel_DPLR(Lambda, P, P, B, C, step=1.0 / L, L=L)
+        self.assertTrue(torch.allclose(K_naive.real, K, atol=1e-5, rtol=1e-5))
+        conv_out = convolve(u, K)
+
+        self.assertTrue(torch.allclose(recurrent_out.real, conv_out.real, atol=1e-5, rtol=1e-5))
+
 
 if __name__ == '__main__':
     unittest.main()
