f4918f29062dcb740f43ebc107d987c04a7f0d9d
CanKeles5 <cankeles22@gmail.com>
2023-05-07 13:38:54 +0300

    Starter py file

---
+++ starter.py
@@ -0,0 +1,23 @@
+'''
+Started code for sashimi
+'''
+
+import torch
+
+
+
+class sashimi_dummy(torch.nn.Module):
+    def __init__(self):
+        pass
+        
+    def forward(self, x):
+    
+        return x
+
+
+
+if __name__ == '__main__':
+    #
+    #
+    
+    
