f48564b3da9a01b80f7774b582ac1f2a75b45c5c
necrashter <iiilker99@gmail.com>
2023-05-14 14:02:04 +0300

    Check if all sample rates are the same in WavDataset, sort by filename

--- wav_dataset.py
+++ wav_dataset.py
@@ -1,6 +1,7 @@
 """
 Dataset creation & data preprocessing for WAV audio files.
 """
+import torch
 import glob
 import os
 from scipy.io import wavfile
@@ -19,17 +20,27 @@ class WavDataset(Dataset):
             transform (callable, optional): Optional transform to be applied
                 on a sample.
         """
-        
         self.root_dir = root_dir
         self.transform = transform
-        self.wav_pths = glob.glob(os.path.join(root_dir, "*.wav"))
-        self.wavs = [wavfile.read(path) for path in self.wav_pths]
+
+        self.wav_paths = sorted(glob.glob(os.path.join(root_dir, "*.wav")))
+        wavs = [wavfile.read(path) for path in self.wav_paths]
+
+        # Check if all sample rates are the same
+        sample_rates = [i[0] for i in wavs]
+        sample_rate = sample_rates[0]
+        sample_rates = torch.Tensor(sample_rates)
+        if not torch.all(sample_rate == sample_rates):
+            raise ValueError("Not all WAV files have the same sample rate!")
+        self.sample_rate = sample_rate
+
+        self.wavs = [i[1] for i in wavs]
     
     def __len__(self):
-        return len(self.wav_pths)
+        return len(self.wavs)
 
     def __getitem__(self, idx):
-        sampling_rate, audio = self.wavs[idx]
+        audio = self.wavs[idx]
         
         if self.transform:
             audio = self.transform(audio)
