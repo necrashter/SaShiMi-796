2f748531a0eef3a69aab3205f738c52b96f1989e
necrashter <iiilker99@gmail.com>
2023-05-15 11:37:53 +0300

    Implement CausalPooledResidual

--- sashimi.py
+++ sashimi.py
@@ -67,6 +67,67 @@ class CausalUpPool(UpPool):
         return self.no_shift(x)
 
 
+class CausalPooledResidual(nn.Module):
+    """
+    A sequential block wrapped between DownPool and UpPool layers with a residual connection
+    from its beginning to the end.
+    """
+    def __init__(self,
+                 layers,
+                 signal_dim: int,
+                 pooling_factor: int = 4,
+                 expansion_factor: int = 2,
+                ):
+        super().__init__()
+        self.sequential = Sequential(*layers)
+        self.down_pool = DownPool(signal_dim, pooling_factor, expansion_factor)
+        self.up_pool = CausalUpPool(signal_dim, pooling_factor, expansion_factor)
+        self.signal_dim = signal_dim
+        self.pooling_factor = pooling_factor
+
+    def forward(self, x):
+        return self.up_pool(self.sequential(self.down_pool(x))) + x
+
+    def get_recurrent_runner(self):
+        """
+        Discretize the model with given L and return a function that maps state and input to
+        the new state and input.
+        """
+        input_cache = []
+        # First block will be up-pooled from zeros due to shifting.
+        device = next(iter(self.parameters())).device
+        first = torch.zeros(1, self.up_pool.linear.in_features, device=device)
+        first = self.up_pool.no_shift(first)
+        # first shape: (1, self.pooling_factor, hidden)
+        output_cache = [i for i in first.squeeze(0)]
+        output_cache.reverse()
+        if len(output_cache) != self.pooling_factor:
+            __import__('pdb').set_trace()
+
+        sequential = self.sequential.get_recurrent_runner()
+
+        def f(u):
+            nonlocal sequential, input_cache, output_cache
+            output = output_cache.pop() + u
+            input_cache.append(u)
+
+            if len(input_cache) == self.pooling_factor:
+                x = torch.stack(input_cache)
+                input_cache.clear()
+                output_cache.clear()
+                y = self.down_pool(x).squeeze()
+                y = self.up_pool.no_shift(sequential(y).unsqueeze(0))
+                output_cache = [i for i in y.squeeze(0)]
+                output_cache.reverse()
+
+            if list(u.size()) != list(output.size()):
+                __import__('pdb').set_trace()
+
+            return output
+
+        return f
+
+
 def SaShiMi(input_dim: int,
             hidden_dim: int,
             output_dim: int,
@@ -76,18 +137,19 @@ def SaShiMi(input_dim: int,
            ):
     return Sequential(
         nn.Linear(input_dim, hidden_dim),
-        Residual(
-            DownPool(hidden_dim),
-            Residual(
-                DownPool(2 * hidden_dim),
-                Residual(*[
-                    S4Block(4 * hidden_dim, state_dim, sequence_length // 16)
-                    for _ in range(block_count)
-                ]),
-                CausalUpPool(2 * hidden_dim),
-            ),
-            *[S4Block(2 * hidden_dim, state_dim, sequence_length // 4) for _ in range(block_count)],
-            CausalUpPool(hidden_dim),
+        CausalPooledResidual(
+            signal_dim = hidden_dim,
+            layers = [
+                CausalPooledResidual(
+                    signal_dim = hidden_dim * 2,
+                    layers = [Residual(*[
+                        S4Block(4 * hidden_dim, state_dim, sequence_length // 16)
+                        for _ in range(block_count)
+                    ])],
+                ),
+                *[S4Block(2 * hidden_dim, state_dim, sequence_length // 4)
+                  for _ in range(block_count)],
+            ],
         ),
         *[S4Block(hidden_dim, state_dim, sequence_length) for _ in range(block_count)],
         nn.Linear(hidden_dim, output_dim),
