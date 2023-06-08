88786759344e2e8318488a936564fc5932e9850a
necrashter <iiilker99@gmail.com>
2023-06-08 16:01:16 +0300

    Download MNIST models from Hugging Face

--- download_data.sh
+++ download_data.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-echo Downloading and unzipping the models...
+echo Downloading and unzipping the Youtube Mix models...
 wget https://huggingface.co/necrashter/SaShiMi-796/resolve/main/youtube-mix-models.zip -q --show-progress
 unzip -o youtube-mix-models.zip
+
+echo Downloading and unzipping the MNIST models...
+wget https://huggingface.co/necrashter/SaShiMi-796/resolve/main/mnist-models.zip -q --show-progress
+unzip -o mnist-models.zip
 
 echo
 echo Downloading and unzipping the Youtube Mix dataset...
@@ -10,3 +14,4 @@ wget https://huggingface.co/necrashter/SaShiMi-796/resolve/main/youtube-mix.zip 
 unzip -o youtube-mix.zip
 
 echo Done!
+echo You can now safely delete the downloaded zip files to save storage space.
